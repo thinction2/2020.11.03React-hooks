0b284e8abc9b960f3129439b1135f0688817b16a
thinction2 <dudwlsehdwk@nate.com>
2021-02-19 16:43:16 +0900

    BackUp

---
+++ HOOKS/useBeforLeave/index.js
@@ -0,0 +1,11 @@
+const App = () => {
+  const begForLife = () => console.log("pls dont leave");
+  useBeforLeave(begForLife);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ HOOKS/useBeforLeave/useBeforLeave.js
@@ -0,0 +1,17 @@
+export const useBeforLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = (e) => {
+    const { clientY } = e;
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => {
+      document.removeEventListener("mouseleave", handle);
+    };
+  }, []);
+};
